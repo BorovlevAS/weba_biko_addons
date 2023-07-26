a2b9fd5bf93911083c8b68a57eda4922d6201509
bambrik-qd <Bambrik@ukr.net>
2023-07-26 10:29:56 +0300

    [ADD](T0621)biko_crm_lead_total: добавлены поля расчеты, выведены на форму

---
+++ biko_crm_lead_total/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    "name": "BIKO: Модуль добавляет в ТЧ часть сделки итоговые суммы",
+    "version": "14.0.1.1.1",
+    "author": "Zhmihova T.N.",
+    "company": "BIKO Solutions",
+    "depends": [
+        "crm",
+        "crm_opportunity_product",
+    ],
+    "data": [
+        "views/crm_lead_views.xml",
+        # "security/ir.model.access.csv",
+    ],
+    "license": "LGPL-3",
+    "installable": True,
+    "application": True,
+    "auto_install": False,
+}
