272bc75c7837a85ab6ee6391d67a1fa1a66db674
bambrik-qd <Bambrik@ukr.net>
2023-08-07 17:01:24 +0300

    [IMX](T0766)biko_crm_lead_report:добавлена групп-ка по кол-ву звонков

--- biko_crm_lead_report/__manifest__.py
+++ biko_crm_lead_report/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "BIKO: Сводный отчет по лидам",
-    "version": "14.0.1.1.1",
+    "version": "14.0.1.2.1",
     "author": "Borovlev A.S.",
     "company": "BIKO Solutions",
     "depends": [
